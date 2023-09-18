08f65fa41c5a7d831deb8fbe2f56ed3036960e40
Kimutai <jankimsingoei@gmail.com>
2023-09-18 12:03:03 +0300

    Done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    
+    def __init__(self,my_list):
+        self.my_list = my_list
+        
+    def match(self,my_lists):
+        my_data = []
+        for letter in my_lists:
+            if sorted(letter) == sorted(self.my_list) and letter != self.my_list:
+                my_data.append(letter)
+        return (my_data)
+    
+       
+
+listen = Anagram('listen')
+listen.match(['enlists', 'google', 'inlets', 'banana'])
